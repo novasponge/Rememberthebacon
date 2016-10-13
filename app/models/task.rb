689aaf819a3c0cdb93d26e4a06598cbfb1c562eb
Zhuoli Zhang <novasponge82@gmail.com>
2016-10-12 23:05:30 -0400

    add due-date search function, style search bar

--- app/models/task.rb
+++ app/models/task.rb
@@ -25,6 +25,27 @@ class Task < ApplicationRecord
 
 
   def self.search_tasks(queryStr)
-    Task.where("tasks.name like '%#{queryStr}%'")
+    queryStr = queryStr.split(" ")
+    name =  queryStr.select do |str|
+      !(/^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]|(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)(?:0?2|(?:Feb))\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/.match(str))
+    end
+
+    date = queryStr.select do |str|
+      /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]|(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)(?:0?2|(?:Feb))\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/.match(str)
+    end
+
+    if name.empty? && date.empty?
+      return Task.where("tasks.name like '%#{name}%'")
+    elsif !name.empty? && !date.empty?
+      date = Date.parse(date.first)
+      name = name.join(" ")
+      return Task.where("tasks.name like '%#{name}%'").where("tasks.due_date = '#{date}'")
+    elsif name.empty?
+      date = Date.parse(date.first)
+      return Task.where("tasks.due_date = '#{date}'")
+    elsif date.empty?
+      name = name.join(" ")
+      return Task.where("tasks.name like '%#{name}%'")
+    end
   end
 end
--- frontend/components/app/app_header.jsx
+++ frontend/components/app/app_header.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { logout } from '../../actions/session_actions';
 import { connect } from 'react-redux';
 import { searchTasks } from '../../actions/task_actions';
+import ClickOutside from 'react-clickoutside-component';
 
 
 class AppHeader extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      queryStr: ""
+      queryStr: "",
+      searchBar: "search-string-area inactive"
     };
 
     this.handleSearchSting = this.handleSearchSting.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSearchClick = this.handleSearchClick.bind(this);
+    this.handleOutSideClick = this.handleOutSideClick.bind(this);
   }
 
 
@@ -23,13 +27,29 @@ class AppHeader extends React.Component {
   handleSubmit(e) {
     e.preventDefault();
     this.props.searchTasks(this.state.queryStr);
+    this.setState({searchBar: "search-string-area inactive"});
+  }
+
+  handleSearchClick (e) {
+    this.setState({searchBar: "search-string-area active"});
+  }
+
+  handleOutSideClick (e) {
+    this.setState({searchBar: "search-string-area inactive"});
   }
 
   render() {
     return(
       <div className="app-header group">
         <form className="search-bar" onSubmit={this.handleSubmit}>
-          <input type='text' className='search-string-area' value={this.state.queryStr} onChange={this.handleSearchSting} />
+          <ClickOutside  onClickOutside={this.handleOutSideClick}>
+            <input type='text'
+                  className={this.state.searchBar}
+                  value={this.state.queryStr}
+                  onChange={this.handleSearchSting}
+                  onClick={this.handleSearchClick}
+                  placeholder="Search by task name or date DD/MM/YYYY"/>
+          </ClickOutside>
           <button className="search-button">
             <i className="fa fa-search" aria-hidden="true"></i>
           </button>
