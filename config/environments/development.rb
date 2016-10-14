1c4e32a5fd51d28e70cb83d7f8c817a14416c93e
Zhuoli Zhang <novasponge82@gmail.com>
2016-10-14 13:48:37 -0400

    change head title

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,14 +28,6 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :letter_opener
 
-  # config.action_mailer.smtp_settings = {
-  #   address: 'smtp.gmail.com',
-  #   port: 587,
-  #   user_name: ENV['gmail_username'],
-  #   password: ENV['gmail_password'],
-  #   authentication: 'plain',
-  #   :enable_starttls_auto: true
-  # }
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
