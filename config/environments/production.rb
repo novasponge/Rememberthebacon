140d78b7a5ceca89191dbb2d925ecd1dc3a7dec4
Zhuoli Zhang <novasponge82@gmail.com>
2016-10-14 12:15:26 -0400

    change domain setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: ENV['MAILGUN_SMTP_SERVER'],
     port: ENV['MAILGUN_SMTP_PORT'],
-    domain: 'rememberthebacon.herokuapp.com',
+    domain: ENV['MAILGUN_DOMAIN'],
     user_name: ENV['MAILGUN_SMTP_LOGIN'],
     password: ENV['MAILGUN_SMTP_PASSWORD'],
     authentication: :plain
