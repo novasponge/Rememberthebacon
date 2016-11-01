42f0b96309968da2b2b69d1a88a881b6e5424d7b
Zhuoli Zhang <novasponge82@gmail.com>
2016-10-31 21:30:42 -0400

    change homepage footer font color

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="home-page-footer">
-      <h4>Find <a href="http://www.zhuolizhang.com">me</a> on</h4>
+      <h4>© Zhuoli Zhang</h4>
       <ul className="footer-icons">
+        <li>
+          <p>Find <a href="http://www.zhuolizhang.com">me</a> on:</p>
+        </li>
         <li>
           <a href="https://github.com/novasponge">
             <i className="fa fa-github-alt footer-icon" aria-hidden="true"></i>
