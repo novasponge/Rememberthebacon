a49ad4ac8f309a9067cf3fe2118db0fbd290cc28
Zhuoli Zhang <novasponge82@gmail.com>
2016-11-28 23:15:48 -0500

    setup scope

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,6 +1,12 @@
 class Api::SessionsController < ApplicationController
   def omni_create
     @user = User.from_omniauth(env['omniauth.auth'])
+    @token = request.env['omniauth.auth']["credentials"]["token"]
+    client = Google::Apis::CalendarV3::CalendarService.new
+    # fix bug here
+    client.authorization.access_token = @token
+    service = client.discovered_api('calendar', 'v3')
+    debugger
     if @user
       log_in!(@user)
       redirect_to root_path
