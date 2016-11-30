9144b8626e2b4fdcf1b4bd22d3b5f1ffa946c6ce
Zhuoli Zhang <novasponge82@gmail.com>
2016-11-30 14:35:03 -0500

    remove secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 OmniAuth.config.full_host = Rails.env.production? ? 'http://www.rememberthebacon.co' : 'http://localhost:3000'
 
-# Rails.application.config.middleware.use OmniAuth::Builder do
-#   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {scope: 'userinfo.email, userinfo.profile, https://www.googleapis.com/auth/calendar', client_options: {ssl: {ca_file: Rails.root.join('cacert.pem').to_s}}}
-# end
-
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '376282486981-rq8mp4h46j06t06ejd9hindfjt49flag.apps.googleusercontent.com', 'kMyKDUYEAbd9Ev-GAn3QMaV2', {scope: 'userinfo.email, userinfo.profile, https://www.googleapis.com/auth/calendar', client_options: {ssl: {ca_file: Rails.root.join('cacert.pem').to_s}}}
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {scope: 'userinfo.email, userinfo.profile, https://www.googleapis.com/auth/calendar', client_options: {ssl: {ca_file: Rails.root.join('cacert.pem').to_s}}}
 end
